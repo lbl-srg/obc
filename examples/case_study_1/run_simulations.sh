c460166886ac3ae651d9330a1ddef81df850c0c1
Michael Wetter <mwetter@lbl.gov>
2017-09-22 10:34:20 -0700

    Updated script

--- examples/case_study_1/run_simulations.sh
+++ examples/case_study_1/run_simulations.sh
@@ -4,7 +4,7 @@
 # with your model
 #############################################################
 import os
-BRANCH=master
+BRANCH="master"
 CWD = os.getcwd()
 
 
@@ -27,19 +27,15 @@ def create_working_directory():
     import os
     import tempfile
     import getpass
-    curDir = os.path.abspath(".")
-    ds = curDir.split(os.sep)
-    dirNam = ds[len(ds) - 1]
-    worDir = os.path.join(tempfile.mkdtemp(
-        prefix='tmp-simulator-case_study-' + getpass.getuser() + '-'), dirNam)
+    worDir = tempfile.mkdtemp( prefix='tmp-simulator-case_study-' + getpass.getuser() )
+#    print("Created directory {}".format(worDir))
     return worDir
 
-def checkout_repository(num, working_directory):
+def checkout_repository(working_directory):
     import os
     from git import Repo
     import git
     git_url = "https://github.com/lbl-srg/modelica-buildings"
-#    print("checking out to {}".format(working_directory))
     Repo.clone_from(git_url, working_directory)
     g = git.Git(working_directory)
     g.checkout(BRANCH)
@@ -50,19 +46,23 @@ def _simulate(spec):
 
     from buildingspy.simulate.Simulator import Simulator
 
+    wor_dir = create_working_directory()
+
     model = spec["model"]
-    out_dir = spec["output_directory"]
+    out_dir = os.path.join(wor_dir, spec["output_directory"])
     start_time = spec["start_time"]
     stop_time   = spec["stop_time"]
-    wor_dir = create_working_directory()
+
     # Update MODELICAPATH to get the right library version
-    os.environ["MODELICAPATH"] = ":".join([CWD, out_dir])
+    os.environ["MODELICAPATH"] = ":".join([spec['lib_dir'], out_dir])
 
-    checkout_repository(num, wor_dir)
 
-    out_dir = os.path.join(wor_dir, "out_dir")
+
+    # Copy the models
+#    print("Copying models from {} to {}".format(CWD, wor_dir))
+    shutil.copytree(os.path.join(CWD, "VAVMultiZone"), os.path.join(wor_dir, "VAVMultiZone"))
     # Change the working directory so that the right checkout is loaded
-    os.chdir(wor_dir)
+    os.chdir(os.path.join(wor_dir, "VAVMultiZone"))
 
     s=Simulator(model, "dymola", outputDirectory=out_dir)
     s.addPreProcessingStatement("OutputCPUtime:= true;")
@@ -72,27 +72,49 @@ def _simulate(spec):
     s.setStopTime(stop_time)
     s.setTolerance(1E-6)
     s.showGUI(False)
-#    print("Starting simulation")
+    print("Starting simulation in {}".format(out_dir))
     s.simulate()
+
+    # Copy results back
+    res_des = os.path.join(CWD, spec["output_directory"])
+    if os.path.isdir(res_des):
+       shutil.rmtree(res_des)
+    print("Copying results to {}".format(res_des))
+    shutil.move(out_dir, res_des)
+
+    # Delete the working directory
+    shutil.rmtree(wor_dir)
 
 ################################################################################
 if __name__=='__main__':
-   from multiprocessing import Pool
+    from multiprocessing import Pool
+    import shutil
 
-   cases = list()
-   cases.append( \
-       {'model': "VAVMultiZone.Example.SimpleTest",
-        "output_directory": "case1",
-        "start_time": 0,
-        "stop_time":  1}
-   cases.append( \
-       {'model': "VAVMultiZone.Example.SimpleTest",
-        "output_directory": "case1",
-        "start_time": 0,
-        "stop_time":  2}
+    cases = list()
+    cases.append( \
+        {'model': "VAVMultiZone.Example.SimpleTest",
+         "output_directory": "case1",
+         "start_time": 0,
+         "stop_time":  1})
+    cases.append( \
+        {'model': "VAVMultiZone.Example.SimpleTest",
+         "output_directory": "case2",
+         "start_time": 0,
+         "stop_time":  2})
 
     # Number of parallel processes
     nPro = 10
     po = Pool(nPro)
+
+    lib_dir = create_working_directory()
+    checkout_repository(lib_dir)
+    # Add the directory where the library has been checked out
+    for case in cases:
+        case['lib_dir'] = lib_dir
+
     # Run all cases
     po.map(_simulate, cases)
+    #shutil.rmtree(lib_dir)
+
+    # Delete the checked out repository
+    shutil.rmtree(lib_dir)
