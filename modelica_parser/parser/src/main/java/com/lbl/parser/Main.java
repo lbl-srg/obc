0d4717017cbd733a29cc7a7892879d839bc7e84e
Michael Wetter <mwetter@lbl.gov>
2017-12-13 15:51:03 -0800

    Added exit statements [ci skip]

--- modelica_parser/parser/src/main/java/com/lbl/parser/Main.java
+++ modelica_parser/parser/src/main/java/com/lbl/parser/Main.java
@@ -16,24 +16,24 @@ import java.util.ArrayList;
 /**
  * Created by JayHu on 07/21/2017
  */
-public class Main {  	
-	
-	private static final String modelicaSourceCode(String filePath) {    			
+public class Main {
+
+	private static final String modelicaSourceCode(String filePath) {
 		return readLineByLineJava8(filePath);
 	}
-	
-	private static String readLineByLineJava8(String filePath) {   	
+
+	private static String readLineByLineJava8(String filePath) {
 		StringBuilder contentBuilder = new StringBuilder();
 		Path path = Paths.get(filePath);
-		try (Stream<String> lines = Files.lines(path, StandardCharsets.UTF_8)) {           
+		try (Stream<String> lines = Files.lines(path, StandardCharsets.UTF_8)) {
 			lines.forEach(s -> contentBuilder.append(s).append("\n"));
 		}
 		catch (IOException e) {
 			e.printStackTrace();
-		}            
+		}
 		return contentBuilder.toString();
 	}
-	
+
 	private String fileNameToSearch;
 	private List<String> result = new ArrayList<String>();
 	public String getFileNameToSearch() {
@@ -45,39 +45,38 @@ public class Main {
 	public List<String> getResult() {
 		return result;
 	}
-    
-    public static void main(String[] args) {     	
+
+    public static void main(String[] args) {
     	String dirToBeSearched = System.getProperty("user.dir").concat("/" + "modelica_models");
-    	Main fileSearch = new Main(); 
+    	Main fileSearch = new Main();
     	fileSearch.searchDirectory(new File(dirToBeSearched), ".mo");
-    	
-    	int count = fileSearch.getResult().size();    	
+
+    	int count = fileSearch.getResult().size();
     	if (count == 0) {
-    		System.out.println("\nNo result found!");
-    	} else {  	
-    		System.out.println("\nFound " + count + " result!\n");
-    		System.out.printf("%n============ Start parsing ...... ============");
-    	    for (String matched : fileSearch.getResult()){
+    		System.err.println("\nNo .mo files found.");
+			System.exit(1);
+    	} else {
+   	    for (String matched : fileSearch.getResult()){
     	    	int dotInd = matched.indexOf(".");
     	    	String nameForJson = matched.substring(0, dotInd).replace("modelica_models", "json_files").concat(".json");
     	    	int dirRootEnd = nameForJson.lastIndexOf("/");
     	    	String dirRootStr = nameForJson.substring(0, dirRootEnd);
-    	    	
-    	    	Path path = Paths.get(dirRootStr);    	    	
+
+    	    	Path path = Paths.get(dirRootStr);
     	    	File dirRoot = new File(dirRootStr);
     	    	if (! dirRoot.exists()) {
     	    		try {
     	    		    Files.createDirectories(path);
     	    		} catch (IOException e) {
     	    		    System.err.println("Cannot create directories - " + e);
+						System.exit(1);
     	    		}
-    	    	}    	    	    	    	    	
-    	    	//System.out.printf("%nrootWithName: '%s'", dirRoot); 	     		
-    	    	final Stored_definition antlrParseOut = new VisitorOrientedParser().parse(modelicaSourceCode(matched)); 
-    	    	//Gson gson = new GsonBuilder().setPrettyPrinting().create(); 
-    	    	Gson gson = new Gson();   	    	
-    	    	System.out.printf("%n '%s'", matched);
-    	    	final String jsonOut = gson.toJson(antlrParseOut);       
+    	    	}
+    	    	//System.out.printf("%nrootWithName: '%s'", dirRoot);
+    	    	final Stored_definition antlrParseOut = new VisitorOrientedParser().parse(modelicaSourceCode(matched));
+    	    	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    	    	//Gson gson = new Gson();
+    	    	final String jsonOut = gson.toJson(antlrParseOut);
     	    	try {
     	    		File newTextFile = new File(nameForJson);
     	    		FileWriter fw = new FileWriter(newTextFile);
@@ -86,12 +85,13 @@ public class Main {
 
     	    	} catch (IOException iox) {
     	    		iox.printStackTrace();
-    	    	}              	    	
-    	    }	 
-    	    System.out.printf("%n%n==== Done parsing! ====%n");
+					System.exit(1);
+    	    	}
+    	    }
+			System.exit(0);
     	}
     }
-    
+
     public void searchDirectory(File directory, String fileNameToSearch) {
     	setFileNameToSearch(fileNameToSearch);
     	if (directory.isDirectory()) {
@@ -102,19 +102,18 @@ public class Main {
       }
     private void search(File file) {
     	if (file.isDirectory()) {
-    	  System.out.println("Searching directory ... " + file.getAbsoluteFile());
-            //allowed to read this directory?
+           //allowed to read this directory?
     	    if (file.canRead()) {
     	    	for (File temp : file.listFiles()) {
     	    		if (temp.isDirectory()) {
     	    			search(temp);
-    	    		} else {  
+    	    		} else {
     	    			int dotInd = temp.getName().lastIndexOf(".");
     	    			int nameLen = temp.getName().length();
     	    			// some files don't have any extension so that dotInd will be -1
     	    			int indForExt = dotInd==(-1) ? 0 : dotInd;
     	    			//if (getFileNameToSearch().equals(temp.getName().toLowerCase())) {
-    	    		    if (getFileNameToSearch().equals(temp.getName().substring(indForExt,nameLen))) {	
+    	    		    if (getFileNameToSearch().equals(temp.getName().substring(indForExt,nameLen))) {
     	    				result.add(temp.getAbsoluteFile().toString());
     	    			}
     	    		}
