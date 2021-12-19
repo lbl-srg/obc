7f9284ad2b7738b0fe7a3217f29be2017b9503e2
Michael Wetter <mwetter@lbl.gov>
2021-12-19 08:26:04 -0800

    Updated sizing data, changed to MSL 4

--- examples/case_study_4_fullHVAC/install.sh
+++ examples/case_study_4_fullHVAC/install.sh
@@ -28,9 +28,9 @@ get_repo(){
 }
 
 echo "Updating Modelica Buildings Library"
-get_repo $TMP_MBL https://github.com/lbl-srg/modelica-buildings.git 9bfaaf3d913e904fc67badad35bd39db1e72cb98
+get_repo $TMP_MBL https://github.com/lbl-srg/modelica-buildings.git 9ca66680df40e4a4b96a9394a1b6f5de5d135d30 # master, uses MSL 4.0.0
 echo "Updating BuildingsPy"
-get_repo $TMP_BUP https://github.com/lbl-srg/BuildingsPy.git 5a133c89f9f759993e90e99fb445c772ddb44bfe # this is release v3.0.0
+get_repo $TMP_BUP https://github.com/lbl-srg/BuildingsPy.git dec3decca31fcd84aecbe3a83f8c060ada7f6156 # master
 
 echo "Don't forget to set export MODELICAPATH=${TMP_MBL}"
 echo "Don't forget to set export PYTHONPATH=${TMP_BUP}:$PYTHONPATH"
